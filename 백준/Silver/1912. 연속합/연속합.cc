39ce597775e04f3cd99602d8fa1e789c65b6dd0e
Sohee Kim <wneh2023@naver.com>
2025-11-21 21:23:56 +0900

    Update 연속합.cc

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,12 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int n, result;
-int arr[100001], d[100001], s[100001];
+int n, result, arr[100001], d[100001], s[100001];
 
 int main(){
-	cin.tie(0);
-	cout.tie(0);
-	ios::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
 	arr[0] = 0;
@@ -16,15 +14,18 @@ int main(){
 		cin >> arr[i];
 	}
 
+    // 현재까지 누적합
 	for (int i = 1; i <= n; i++) {
 		s[i] = s[i - 1] + arr[i];
 	}
 
+    // 이전까지의 최소 누적합
 	d[0] = 1000;
 	for (int i = 1; i <= n; i++) {
 		d[i] = min(s[i - 1], d[i - 1]);
 	}
 
+    // 최대 부분합 = (현재까지 누적합) - (이전까지의 최소 누적합)
 	for (int i = 1; i <= n; i++) {
 		result = max(result, s[i] - d[i]);
 	}
