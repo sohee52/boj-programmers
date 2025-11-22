1b868f72d7e10bc121c434a1681684aa458d051e
Sohee Kim <wneh2023@naver.com>
2025-11-22 21:06:20 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -3,6 +3,5 @@ using namespace std;
 double a, b;
 int main() {
 	cin >> a >> b;
-    double x = a / b;
-    cout << fixed << setprecision(50) << x << "\n";
+    cout << fixed << setprecision(50) << a / b << "\n";
 }
