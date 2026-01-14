8d68b550f516d56deeb3e6a7132b2cd8421d7270
Sohee Kim <wneh2023@naver.com>
2026-01-15 07:35:29 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,5 +1,6 @@
 select c.item_id, c.item_name, c.rarity
-from item_tree as t
-join item_info as p on p.item_id = t.parent_item_id and p.rarity = 'RARE'
-join item_info as c on c.item_id = t.item_id
+from item_info as i
+join item_tree as t on i.item_id = t.parent_item_id
+join item_info as c on t.item_id = c.item_id
+where i.rarity = 'RARE'
 order by c.item_id desc;
