8868565008e56d9ad118b7361516f0d787de6788
Sohee Kim <wneh2023@naver.com>
2026-01-16 02:51:11 +0900

    [Gold IV] Title: 명제 증명, Time: 4 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/2224. 명제 증명/명제 증명.cc
+++ 백준/Gold/2224. 명제 증명/명제 증명.cc
@@ -5,6 +5,7 @@ const int INF = 1e9;
 const int MAX = 52;
 
 int dist[MAX][MAX];
+vector<pair<int, int>> ans;
 
 int idx(char a) {
     if(a >= 'A' && a <= 'Z') return a - 'A';
@@ -17,15 +18,16 @@ int main() {
 
     for (int i = 0; i < MAX; i++) {
         for (int j = 0; j < MAX; j++) {
-            dist[i][j] = (i == j ? 0 : INF);
+            if (i == j) dist[i][j] = 0;
+            else dist[i][j] = INF;
         }
     }
 
     for (int i = 0; i < n; i++) {
-        char a, b; string arrow;
-        cin >> a >> arrow >> b;
-        int u = idx(a), v = idx(b);
-        dist[u][v] = 1;
+        char u, v; string a;
+        cin >> u >> a >> v;
+        int iu = idx(u); int iv = idx(v);
+        dist[iu][iv] = 1;
     }
 
     for (int k = 0; k < MAX; k++) {
@@ -38,20 +40,18 @@ int main() {
         }
     }
 
-    vector<pair<int,int>> ans;
     for (int i = 0; i < MAX; i++) {
         for (int j = 0; j < MAX; j++) {
             if (i == j) continue;
             if (dist[i][j] != INF)
-                ans.emplace_back(i, j);
+                ans.push_back({i, j});
         }
     }
 
     cout << ans.size() << "\n";
-    for (auto &p : ans) {
-        int x = p.first, y = p.second;
-        char cu = (x < 26 ? 'A' + x : 'a' + (x - 26));
-        char cv = (y < 26 ? 'A' + y : 'a' + (y - 26));
+    for (auto &[u, v] : ans) {
+        char cu = (u < 26 ? 'A' + u : 'a' + (u - 26));
+        char cv = (v < 26 ? 'A' + v : 'a' + (v - 26));
         cout << cu << " => " << cv << "\n";
     }
 
