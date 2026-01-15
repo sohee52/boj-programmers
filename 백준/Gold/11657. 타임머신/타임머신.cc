dbee58b455c0c6e3be3120493badac6dab607635
Sohee Kim <wneh2023@naver.com>
2026-01-15 23:14:28 +0900

    [Gold IV] Title: 타임머신, Time: 8 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -1,35 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const ll INF = 1e12;
+const ll INF = 1e18;
+const int MAX = 504;
 int n, m, a, b, c;
-ll dist[504];
-vector<pair<int, int>> v[6004];
-bool cycle;
+ll dist[MAX];
+queue<int> q;
+vector<pair<int, int>> adj[MAX];
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-    fill(dist, dist + 504, INF);
-	cin >> n >> m;
-    while(m--) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
         cin >> a >> b >> c;
-        v[a].push_back({b, c});
+        adj[a].push_back({b, c});
     }
+
+    fill(dist, dist+MAX, INF);
     dist[1] = 0;
-    for (int i = 0; i < n; i++) {
-        for (int here = 1; here <= n; here++) {
-            for (auto there : v[here]) {
-                int d = there.second;
-                int to = there.first;
-                if (dist[here] != INF && dist[here] + d < dist[to]) {
-                    if(i == n-1) cycle = 1;
-                    dist[to] = dist[here] + d;
+
+    for (int i = 1; i <= n; i++) {
+        for (int u = 1; u <= n; u++) {
+            for (auto[v, w] : adj[u]) {
+                if (dist[u] != INF && dist[v] > dist[u] + w) {
+                    if (i == n) q.push(v);
+                    dist[v] = dist[u] + w;
                 }
             }
         }
     }
-    if (cycle) cout << -1 << "\n";
-    else {
+
+    if (q.size()) {
+        cout << -1 << "\n";
+    } else {
         for (int i = 2; i <= n; i++) {
             cout << (dist[i] == INF ? -1 : dist[i]) << "\n";
         }
