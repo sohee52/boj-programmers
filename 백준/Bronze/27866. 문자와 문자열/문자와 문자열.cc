a900aa9277e1f06126893ad87a34234316591d71
Sohee Kim <wneh2023@naver.com>
2026-03-03 01:06:35 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s; int i;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> s;
+    cin >> i;
+    cout << s[i - 1] << "\n";
+}
