2808b0a9008506fdb74fecb0796b1818ad223e73
Sohee Kim <wneh2023@naver.com>
2026-03-06 19:10:57 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -1,22 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 1004;
-int n, a[MAX], dp[MAX], ans;
+int n, a[1004], ans, dp[1004];
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr);
 	cin >> n;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
+        dp[i] = a[i];
+        for (int j = 0; j < i; j++) {
+            if (a[i] > a[j]) dp[i] = max(dp[i], dp[j] + a[i]);
+        }
     }
 
-    for (int i = 1; i <= n; i++) {
-        dp[i] = a[i];
-        for (int j = i-1; j >= 1; j--) {
-            if (a[i] > a[j]) {
-                dp[i] = max(dp[i], dp[j]+a[i]);
-            }
-        }
+    for (int i = 0; i < n; i++) {
         ans = max(ans, dp[i]);
     }
 
