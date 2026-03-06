51c0c37480301637b612f8ff6ec2cc93eafa69f9
Sohee Kim <wneh2023@naver.com>
2026-03-06 19:51:43 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,31 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, a, b, visited[204], cnt;
-vector<int> v[204];
+int n, k, a, b, cnt;
+vector<int> adj[104];
 queue<int> q;
-void bfs(int here) {
-    queue<int> q;
-    visited[here] = 1;
-    q.push(here);
-    while(q.size()) {
-        int here = q.front(); q.pop();
-        for (int there : v[here]) {
-            if (visited[there]) continue;
-            visited[there] = 1; cnt++;
-            q.push(there);
+bool visited[104];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> n >> k;
+    for (int i = 0; i < k; i++) {
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    q.push(1);
+    visited[1] = 1;
+    while (q.size()) {
+        int x = q.front(); q.pop();
+        for (int nx : adj[x]) {
+            if (visited[nx]) continue;
+            visited[nx] = 1;
+            q.push(nx);
+            cnt++;
         }
     }
 
-}
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        cin >> a >> b;
-        v[a].push_back(b);
-        v[b].push_back(a);
-    }
-    bfs(1);
     cout << cnt << "\n";
 }
